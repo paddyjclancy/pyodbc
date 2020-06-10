000e89ba0a5594707b27087d5501e7804ae24015
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-10 16:48:50 +0100

    Insert method and documentation complete

--- n_subclass_product_table.py
+++ n_subclass_product_table.py
@@ -3,15 +3,15 @@ from n_class_northwind import *
 
 class ProductTable(NorthwindCon):
 
-    # Get one
+    # Get one row of data - specified
     def fetch_by_product_id(self, p_id):
         return self.sql_query('SELECT * FROM Products WHERE ProductID=' + str(p_id)).fetchone()
 
-    # Get all
+    # Get all data from table
     def fetch_all(self):
         return self.sql_query('SELECT * FROM Products').fetchall()
 
-    # Insert
+    # Insert - requires a product name, price, and quantity in stock
     def insert_row(self, product_name, product_price, product_stock):
         return self.sql_query(f"""
         INSERT INTO Products
